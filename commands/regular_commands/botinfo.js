756f00dabcc4439a1dc6db319eeb995562007eeb
Cyryll Galon <galoncyryll@gmail.com>
2019-05-10 10:19:52 -0700

    update botinfo;

--- commands/regular_commands/botinfo.js
+++ commands/regular_commands/botinfo.js
@@ -1,15 +1,14 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    const forHEad = bot.emojis.find(emoji => emoji.name === "4HEad").toString();
+    const forHEad = bot.emojis.find(emoji => emoji.name === "4HEad");
     let botEmbed = new Discord.RichEmbed()
     .setDescription('Bot Information')
     .setColor('#00b22c')
     .setThumbnail(bot.user.displayAvatarURL)
-    .addField(bot.user.username, `Just a bot ${forHEad}`)
+    .addField(bot.user.username, `Multi-purpose bot for discord ${forHEad}`)
     .addField('Created On', bot.user.createdAt)
-    .setFooter('Bot made by cycycy | github repo: github.com/galoncyryll/cycycy-bot')
-    .addBlankField(true);
+    .setFooter('Bot made by cycycy | github repo: github.com/galoncyryll/cycycy-bot', bot.user.displayAvatarURL);
 
     return message.channel.send(botEmbed);
 }
