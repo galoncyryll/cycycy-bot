9ad4a62aac3ed4b2dfecaf98c70ba6a4707d2d25
Cyryll Galon <galoncyryll@gmail.com>
2019-06-06 15:26:14 -0700

    update dictionary command

--- commands/regular_commands/dictionary.js
+++ commands/regular_commands/dictionary.js
@@ -1,33 +1,42 @@
 const Discord = require('discord.js');
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args, NaM) => {
     if(args[0] === "help") {
         message.channel.send("```Usage: !=dictionary <word>```");
         return;
     }
     let word = args.join(' ');
-    fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=4f364930-85c3-4dde-9714-71a1c8f3b97f`)
+    fetch(`https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=${process.env.DICTIONARY_KEY}`)
         .then(res => res.json())
         .then(definition => {
-            
+            console.log(definition)
+            if(!definition || !definition[0].meta) {
+                let wordEmbed = new Discord.RichEmbed()
+                .setColor('#ff0000')
+                .setTitle(`Can't find the word you are looking for`)
+                .setThumbnail('https://www.flaticon.com/premium-icon/icons/svg/78/78924.svg')
+                .addField('Did you mean these words?', definition.join(', '))
+                .setFooter('Powered by Merriam Webster API', 'https://www.merriam-webster.com/assets/mw/static/app-standalone-images/MW_logo.png')
+                return message.channel.send(wordEmbed);
+            }
             definition.forEach(async defs => {
-                
                 let functionalLabel = defs.fl;
                 let wordDef = defs.shortdef;
                 let example = defs.def[0].sseq[0][0][1].dt[1][1][0].t
                 // join args
-                let joinedSyns= synonyms[0].join(', ');
+                let joinedSyns= defs.meta.syns[0] ? defs.meta.syns[0].join(', ') : null;
 
                 let wordEmbed = new Discord.RichEmbed()
                     .setColor("#004bc4")
-                    .setTitle(word.charAt(0).toUpperCase() + word.slice(1))
-                    .addField('Synonyms: ', synonyms ? joinedSyns : null )
+                    .setTitle(`Word: ${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+                    .addField('Synonyms: ', joinedSyns)
                     .addField('Functional Label: ', functionalLabel)
                     .addField('Definition: ', wordDef)
                     .addField('Example: ', '```' + example +'```')
+                    .setFooter('Powered by Merriam Webster API', 'https://www.merriam-webster.com/assets/mw/static/app-standalone-images/MW_logo.png')
                 await message.channel.send(wordEmbed);
             })
-        }).catch(err => message.reply(err));
+        }).catch(message.channel.send(`No results found.`));
 }
 
 module.exports.help = {
