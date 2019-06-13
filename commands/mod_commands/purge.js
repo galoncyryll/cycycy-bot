f2bf2298d52507f45ac4b62fa82c1897ffb03694
Cyryll Galon <galoncyryll@gmail.com>
2019-06-13 13:11:32 -0700

    add purge log

--- commands/mod_commands/purge.js
+++ commands/mod_commands/purge.js
@@ -1,13 +1,15 @@
+const Discord = require('discord.js');
 const mongoose = require('mongoose');
 const Cmd = require('../../models/customCommandsDB');
 const Mods = require('../../models/modDBtest');
+const db = require('../../settings/databaseImport');
 
 module.exports.run = async (bot, message, args, NaM) => {
     if(args[0] === "help") {
         message.channel.send("```Usage: !=purge <number_of_messages>```");
         return;
     }
-    message.delete();
+    await message.delete();
 
     Mods.findOne({ serverID: message.guild.id }).then(res => {
         if(res) {
@@ -16,7 +18,35 @@ module.exports.run = async (bot, message, args, NaM) => {
                 if(!args[0]) return message.reply(`Please add number of messages ${NaM}`);
                 if(isNaN(args[0])) return message.reply(`Please use number as arguments. ${NaM}`);
 
-                return message.channel.bulkDelete(args[0])
+                return message.channel.bulkDelete(args[0]).then(messages => {
+                    db.Logger.findOne({ serverID: message.guild.id }).then(logRes => {
+                        if(logRes.isEnabled && logRes.isEnabled === 'enable') {
+                            messages.map(messagesItem => {
+                                let logEmbed = new Discord.RichEmbed()
+                                    .setColor('#ff0000')
+                                    .setAuthor(`[PURGED] | ${messagesItem.author.username}`, messagesItem.author.avatarURL)
+                                    .addField('User', `<@${messagesItem.author.id}>`, true)
+                                    .addField('Reason', `Purged by <@${message.author.id}>`, true)
+                                    .addField('Message', messagesItem.content)
+                                    .setFooter(`MESSAGE ID: ${messagesItem.id}`)
+                                    .setTimestamp();
+
+                                return bot.channels.get(logRes.logChannelID).send(logEmbed);
+                            });
+                            // let logEmbed = new Discord.RichEmbed()
+                            //     .setColor('#ff0000')
+                            //     .setAuthor(`[MESSAGE_DELETE] | ${message.author.username}`, message.author.avatarURL)
+                            //     .addField('User', `<@${message.author.id}>`, true)
+                            //     .addField('Reason', logArgs, true)
+                            //     .addField('Message', message.content)
+                            //     .setFooter(`MESSAGE ID: ${message.id}`)
+                            //     .setTimestamp();
+            
+                            // return bot.channels.get(logRes.logChannelID).send(messages.content);
+
+                        }
+                    }).catch(console.log);
+                })
                 
             } else {
                 return message.reply(`You don't have permission for this command ${NaM}`);
