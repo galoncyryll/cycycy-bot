d5a47a2c2327267af9df985263466f91a9c11a48
Cyryll Galon <galoncyryll@gmail.com>
2019-07-27 16:11:14 -0700

    fixed and add dynamic embed color for weather

--- commands/regular_commands/weather.js
+++ commands/regular_commands/weather.js
@@ -1,25 +1,37 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
+const path = require('path');
+const getColors = require('get-image-colors');
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args, NaM) => {
   const joinedArgs = args.join(' ');
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${joinedArgs}&appid=8d1a465793567f602b025c310a5d8c13`, {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${joinedArgs}&units=metric&appid=8d1a465793567f602b025c310a5d8c13`, {
     method: 'get',
     headers: { 'content-type': 'application/json' },
   })
     .then(res => res.json())
     .then((weather) => {
-      console.log(weather);
-      const { name } = weather;
-      const weatherEmbed = new Discord.RichEmbed()
-        .setAuthor(`Current Weather Forecast for ${name}`)
-        .setThumbnail(`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
-        .setTitle('Weather')
-        .setDescription(weather.weather[0].description);
+      if (weather.cod === '404') return message.reply(`City not found ${NaM}`);
+      const {
+        name, sys: { country }, main: { temp, humidity }, clouds: { all }, wind: { speed },
+      } = weather;
 
-
-      return message.channel.send(weatherEmbed);
+      return getColors(path.join(__dirname, `./weatherimg/${weather.weather[0].icon}@2x.png`))
+        .then((colors) => {
+          const weatherEmbed = new Discord.RichEmbed()
+            .setAuthor(`Live Weather Forecast for ${name}, ${country}`)
+            .setThumbnail(`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`)
+            .setTitle('Weather')
+            .setDescription(weather.weather[0].description)
+            .addField('Temperature', `${temp}°C`, true)
+            .addField('Humidity', `${humidity}%`, true)
+            .addField('Wind Speed', `${speed}m/s`, true)
+            .addField('Cloudiness', `${all}%`, true)
+            .setColor(colors[0].hex());
+          return message.channel.send(weatherEmbed);
+        })
+        .catch(err => message.reply(`\`${err}\``));
     });
 };
 
